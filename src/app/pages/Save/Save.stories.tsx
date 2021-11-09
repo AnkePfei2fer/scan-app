aff2afe3ab916ee04d0ee951cc6611dda3b69ef7
Anke <pfeiffer.anke@outlook.com>
2021-11-09 16:53:46 +0100

    :sparkles: Add save Page

---
+++ src/app/pages/Save/Save.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReadAndSave from './Save';
+
+export default { component: ReadAndSave, title: 'Pages/Save' };
+export const Default = () => <ReadAndSave />;
---
+++ src/app/pages/Save/Save.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageInput from '../../components/imageInput/imageInput';
+
+function ReadAndSave() {
+  return (
+    <div>
+      <ImageInput />
+      <button>Read and Save</button>
+    </div>
+  );
+}
+
+export default ReadAndSave;
