4e11d6c394a7316909052f47bd58870abb7daa92
Anke <pfeiffer.anke@outlook.com>
2021-11-09 12:21:35 +0100

    :lipstick: Add components to styleguide

--- src/app/Styleguide.tsx
+++ src/app/Styleguide.tsx
@@ -1,7 +1,59 @@
+import { SSL_OP_SINGLE_DH_USE } from 'constants';
 import React from 'react';
 
 function Styleguide() {
-  return <div>Styleguide</div>;
+  return (
+    <div>
+      <h2> colors </h2>
+      <ul>
+        <li style={{ color: 'var(--color-primary)' }}>primary color</li>
+        <li style={{ color: 'var(--color-secondary)' }}>secondary color</li>
+        <li style={{ color: 'var(--color-tertiary)' }}>tertiary color</li>
+        <li style={{ color: 'var(--color-text-primary)' }}>
+          text primary color
+        </li>
+        <li style={{ color: 'var(--color-text-secondary)' }}>
+          text secondary color
+        </li>
+      </ul>
+      <h2> text </h2>
+      <h1 style={{ color: 'var(--color-primary)' }}>Headline 1</h1>
+      <h2>Headline 2</h2>
+      <p style={{ fontSize: '1.125rem' }}> filename </p>
+      <p> link text </p>
+      <h2> fields, buttons, arrows </h2>
+      <div
+        style={{
+          background: 'var(--color-tertiary)',
+          borderRadius: '10px',
+          color: 'var(--color-text-secondary)',
+          textAlign: 'center',
+          padding: '0.5rem',
+          width: '200px',
+          border: 'solid var(--color-text-secondary) 2px',
+          margin: '2rem',
+        }}
+      >
+        input text field
+      </div>
+      <div
+        style={{
+          background: 'var(--color-primary)',
+          borderRadius: '10px',
+          color: 'var(--color-text-primary)',
+          textAlign: 'center',
+          padding: '0.5rem',
+          width: '200px',
+          border: 'solid black 2px',
+          margin: '2rem',
+          fontSize: '1,25rem',
+          fontWeight: 800,
+        }}
+      >
+        button
+      </div>
+    </div>
+  );
 }
 
 export default Styleguide;
