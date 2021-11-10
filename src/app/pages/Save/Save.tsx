a072e1302fa04a5188f63c087c5ac4e7a440a0c1
Anke <pfeiffer.anke@outlook.com>
2021-11-10 11:34:55 +0100

    :sparkles: Install Tesseract and add scan functionality

--- src/app/pages/Save/Save.tsx
+++ src/app/pages/Save/Save.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import styles from './Save.module.css';
 import ImageInput from '../../components/imageInput/imageInput';
+import Tesseract from 'tesseract.js';
 
 function ReadAndSave() {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  // const [text, setText] = useState<string | null>(null);
 
   return (
     <div className={styles.saveContainer}>
@@ -11,7 +13,17 @@ function ReadAndSave() {
       {imageUrl ? (
         <>
           <input type="text" placeholder="Enter file name"></input>
-          <button className={styles.button} disabled={imageUrl === null}>
+          <button
+            className={styles.button}
+            onClick={() => {
+              Tesseract.recognize(imageUrl, 'eng', {
+                logger: (message) => console.log(message.progress),
+              }).then((result) => {
+                const text = result.data.text;
+                console.log(text);
+              });
+            }}
+          >
             Read and Save
           </button>
         </>
