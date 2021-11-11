ca9f6adc44145e2150b4ec155f9f92dcb9181598
Anke <pfeiffer.anke@outlook.com>
2021-11-11 10:13:39 +0100

    :sparkles: Save file with title and text

--- src/app/components/AddDocumentForm/AddDocumentForm.tsx
+++ src/app/components/AddDocumentForm/AddDocumentForm.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 type AddDocumentFormProps = { text: string };
 
-export default function AddDocumentForm({ text }: AddDocumentFormProps) {
-  // const [title, setTitle] = useState('');
+export default function AddDocumentForm({
+  text,
+}: AddDocumentFormProps): JSX.Element {
+  const [title, setTitle] = useState('');
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const document = { title, text };
+    fetch('https://json-server.machens.dev/docs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(document),
+    });
+  };
 
   return (
-    <form>
-      <input type="text" placeholder="Enter file name"></input>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter file name"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      ></input>
+      <button>Save file</button>
     </form>
   );
 }
