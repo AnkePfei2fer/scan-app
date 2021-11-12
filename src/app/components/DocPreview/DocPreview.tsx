df79cf91683148622898567b0fa11e84d72d46b6
Anke <pfeiffer.anke@outlook.com>
2021-11-12 12:01:43 +0100

    :lipstick: Style DocPreview component

--- src/app/components/DocPreview/DocPreview.tsx
+++ src/app/components/DocPreview/DocPreview.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './DocPreview.module.css';
 
-export default function DocPreview(title: string): JSX.Element {
+export default function DocPreview(): JSX.Element {
   return (
-    <>
+    <div className={styles.fileList}>
       <article className={styles.filePreview}>
         <img className={styles.fileIcon} src="../src/assets/page.png" />
         <a className={styles.linkToFile} href={'#'}>
@@ -22,6 +22,6 @@ export default function DocPreview(title: string): JSX.Element {
           file 3
         </a>
       </article>
-    </>
+    </div>
   );
 }
