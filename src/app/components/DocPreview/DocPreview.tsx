8b58f69a06c97dc927419e3fd46722d8d16a80ed
Anke <pfeiffer.anke@outlook.com>
2021-11-12 16:28:36 +0100

    :recycle: Move fetch function to FileBrowser Page

--- src/app/components/DocPreview/DocPreview.tsx
+++ src/app/components/DocPreview/DocPreview.tsx
@@ -1,34 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './DocPreview.module.css';
-import fetchDocuments from '../../utils/getFiles';
-
-type dataProps = {
-  id: number;
-  title: string;
-  text: string;
-};
 
 export default function DocPreview() {
-  const [data, setData] = useState<dataProps[] | null>(null);
-
-  useEffect(() => {
-    const fetch = async () => {
-      await fetchDocuments().then(setData);
-    };
-    fetch();
-  }, []);
-
   return (
     <div className={styles.fileList}>
-      {data &&
-        data.map((fileName) => (
-          <article className={styles.filePreview} key={fileName.id}>
-            <img className={styles.fileIcon} src="../src/assets/page.png" />
-            <a className={styles.linkToFile} href={'#'}>
-              {fileName.title}
-            </a>
-          </article>
-        ))}
+      <article className={styles.filePreview}>
+        <img className={styles.fileIcon} src="../src/assets/page.png" />
+        <a className={styles.linkToFile} href={'#'}>
+          filename
+        </a>
+      </article>
     </div>
   );
 }
--- src/app/pages/FileBrowser/FileBrowser.tsx
+++ src/app/pages/FileBrowser/FileBrowser.tsx
@@ -1,6 +1,38 @@
-import React from 'react';
-import DocPreview from '../../components/DocPreview/DocPreview';
+import React, { useEffect, useState } from 'react';
+import fetchDocuments from '../../utils/getFiles';
+import styles from './FileBrowser.module.css';
 
 export default function FileBrowser() {
+  type dataProps = {
+    id: number;
+    title: string;
+    text: string;
+  };
+
+  function DocPreview() {
+    const [data, setData] = useState<dataProps[] | null>(null);
+
+    useEffect(() => {
+      const fetch = async () => {
+        await fetchDocuments().then(setData);
+      };
+      fetch();
+    }, []);
+
+    return (
+      <div className={styles.fileList}>
+        {data &&
+          data.map((fileName) => (
+            <article className={styles.filePreview} key={fileName.id}>
+              <img className={styles.fileIcon} src="../src/assets/page.png" />
+              <a className={styles.linkToFile} href={'#'}>
+                {fileName.title}
+              </a>
+            </article>
+          ))}
+      </div>
+    );
+  }
+
   return <DocPreview />;
 }
