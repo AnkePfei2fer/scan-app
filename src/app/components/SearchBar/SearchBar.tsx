90fd0f4ac46d200f4013da0401a6715f5c758dbe
Anke <pfeiffer.anke@outlook.com>
2021-11-15 14:55:07 +0100

    :sparkles: Add search functionality

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import styles from './SearchBar.module.css';
 
-export default function SearchBar() {
+export default function SearchBar(): JSX.Element {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    const response = await fetch(
+      `https://json-server.machens.dev/docs/?title=${searchQuery}`
+    );
+    const searchResults = await response.json();
+    console.log(searchResults);
+    return searchResults;
+  };
+
   return (
-    <form>
-      <input type="text" className={styles.searchField}></input>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className={styles.searchField}
+        onChange={(event) => setSearchQuery(event.target.value)}
+      ></input>
       <button className={styles.searchButton}>Search library</button>
     </form>
   );
