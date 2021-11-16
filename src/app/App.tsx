76227cb24c460898bbc45dc7259cf038f46248eb
Anke <pfeiffer.anke@outlook.com>
2021-11-16 16:29:53 +0100

    :sparkles: Add routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import styles from './App.module.css';
+import { Link } from 'react-router-dom';
+import Scan from './pages/Save/Save';
+import FileBrowser from './pages/FileBrowser/FileBrowser';
 
 function App(): JSX.Element {
-  return <div>grüne Wiese</div>;
+  return (
+    <div>
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+        }}
+      >
+        <Link to="/pages/Save">Scan</Link> |{' '}
+        <Link to="/pages/FileBrowser">Got to library</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './globals.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Scan from './pages/Save/Save';
+import FileBrowser from './pages/FileBrowser/FileBrowser';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+    <Routes>
+      <Route path="/pages/Save/" element={<Scan />} />
+      <Route path="/pages/FileBrowser/" element={<FileBrowser />} />
+    </Routes>
+  </BrowserRouter>,
+  document.querySelector('#root')
+);
