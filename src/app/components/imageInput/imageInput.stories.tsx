88d86cf62518cd0fee133e2b4ae7d7b5e2ca8c2f
AnkePfei2fer <91462624+AnkePfei2fer@users.noreply.github.com>
2022-12-07 13:49:45 +0000

    gh-pages setup

--- src/app/components/imageInput/imageInput.stories.tsx
+++ src/app/components/imageInput/imageInput.stories.tsx
@@ -2,4 +2,10 @@ import React from 'react';
 import ImageInput from './imageInput';
 
 export default { component: ImageInput, title: 'Components/ImageInput' };
-export const Default = () => <ImageInput />;
+export const Default = () => (
+  <ImageInput
+    onUpload={function (): void {
+      throw new Error('Function not implemented.');
+    }}
+  />
+);
